b86d1164d34cc8ba966dc09da8023fd98abab939
Sounak Sengupta <146412118+Sounak-131@users.noreply.github.com>
2023-12-01 01:11:24 +0530

    Add files via upload

---
+++ Assignment 2/jse.js
@@ -0,0 +1,7 @@
+var p1=document.getElementById("p1").value;
+var p2=document.getElementById("p2").value;
+var s=document.getElementById("size").value;
+
+function resetform(){
+    document.getElementById("reset").reset();
+}
